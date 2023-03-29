e817685c8b1d380bc8f4efd961facdcac2ff4c07
Jose Manuel Rojas Villar <villar88@gmail.com>
2023-03-29 15:49:11 -0400

    .

--- src/views/PoolState.vue
+++ src/views/PoolState.vue
@@ -23,4 +23,24 @@ export default {
 
 <style>
 
+    .contentData{
+        display: flex;
+        justify-content: space-between;
+        padding-left: 20rem;
+        padding-right: 30rem;
+    }
+
+    h4{
+        color: white;
+        padding-top: 10px;
+        padding-bottom: 10px;
+    }
+
+    button{
+        background-color: #f1f1f2;
+        padding: 8px;
+        border-radius: 10px;
+        border: 1px solid purple;
+    }
+
 </style>
