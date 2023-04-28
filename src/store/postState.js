1036f19130fd444679840a324f04730dabf55757
Jose Manuel Rojas Villar <villar88@gmail.com>
2023-04-28 16:06:09 -0400

    emit pool a post

--- src/store/postState.js
+++ src/store/postState.js
@@ -13,7 +13,7 @@ export const postStore = defineStore('post', {
         updatePost(postUpdated){
             const x = this.post.find(element => element.id == postUpdated.id)
             let index = x.id
-            this.post[index] = postUpdated
+            this.post[index].title = postUpdated.name
             console.log(this.post[index])
             console.log('Esto es index', index)
         }
